0a170a04796c983d04e502961f509564590580dd
Saranya <sara.29891@gmail.com>
2019-12-19 06:43:03 -0800

    make it better

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,11 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: process.env.PORT ||3306,
+  port:  3306,
   user: "root",
   password: "Carno1234@",
-  database: "burgers_db"
+  database: "burgers_db",
+   
 });
 
 // Make connection.
